eb5423daf25741acdae854ef4bb05a4680b0623f
ammiitt <giriamit432@gmail.com>
2026-07-10 17:02:05 +0530

    Domain layer - pure models, enums, repository interfaces. Fixed all the disperencies

--- ReviewVault.Domain/Interfaces/IUserRepository.cs
+++ ReviewVault.Domain/Interfaces/IUserRepository.cs
@@ -4,8 +4,12 @@ namespace ReviewVault.Domain.Interfaces
 {
     public interface IUserRepository
     {
+        Task<User?> GetByIdAsync(int id);
         Task<User?> GetByEmailAsync(string email);
         Task<bool> ExistsAsync(string email);
         Task<User> CreateAsync(User user);
+        Task<RefreshToken> CreateRefreshTokenAsync(RefreshToken token);
+        Task<RefreshToken?> GetRefreshTokenAsync(string token);
+        Task RevokeRefreshTokenAsync(string token);
     }
 }
